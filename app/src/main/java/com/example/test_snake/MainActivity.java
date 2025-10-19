552744929d9b65847bb4c63d964688277c552f54
Alexis1623 <yadirzm1623@gmail.com>
2025-10-19 11:52:58 -0600

    Revert "Merge branch 'Test' of https://github.com/Alexis1623/Snake_Game into Test"
    This reverts commit 4412035572154d3bed103b0ae164c12876411e13, reversing
    changes made to 4cdaceb2c7aeefc166b8b110ee903ceb4e2d96e4.

--- app/src/main/java/com/example/test_snake/MainActivity.java
+++ app/src/main/java/com/example/test_snake/MainActivity.java
@@ -29,23 +29,20 @@ public class MainActivity extends AppCompatActivity {
         btnStartGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Aquí iniciaremos el juego después
-                // startActivity(new Intent(MainActivity.this, GameActivity.class));
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
             }
         });
 
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Para configuración
-                // startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
             }
         });
 
         btnScores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Para puntuaciones
                 startActivity(new Intent(MainActivity.this, ScoresActivity.class));
             }
         });
@@ -53,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish(); // Cerrar la aplicación
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/test_snake/ScoresActivity.java
+++ app/src/main/java/com/example/test_snake/ScoresActivity.java
@@ -2,36 +2,11 @@ package com.example.test_snake;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ScoresActivity extends AppCompatActivity {
-
-    private RecyclerView rvScores;
-    private ScoresAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
-
-        rvScores = findViewById(R.id.rvScores);
-        rvScores.setLayoutManager(new LinearLayoutManager(this));
-
-        // Datos de ejemplo
-        List<Score> scores = new ArrayList<>();
-        scores.add(new Score(1, "djWoody", 30001));
-        scores.add(new Score(2, "repose", 30000));
-        scores.add(new Score(3, "SVGAMER", 25151));
-        scores.add(new Score(4, "ZOROxJPN", 23183));
-        scores.add(new Score(5, "Barcode", 22400));
-        scores.add(new Score(6, "5744Suika", 18040));
-        scores.add(new Score(7, "GangsterEttienne", 17860));
-        scores.add(new Score(8, "GT", 17820));
-
-        adapter = new ScoresAdapter(scores);
-        rvScores.setAdapter(adapter);
     }
 }
